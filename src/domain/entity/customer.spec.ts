cb4fb9ddb1cc3847bc42a239e707a698d1ecf42d
Jefferson Santos <jeffdeutsch1@gmail.com>
2023-12-28 17:09:33 -0300

    first commit

---
+++ src/domain/entity/customer.spec.ts
@@ -0,0 +1,72 @@
+import Address from "./address"
+import Customer from "./customer"
+
+
+describe("Customer unit tests", () => {
+
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            let customer = new Customer("", "John")
+            
+        }).toThrow("Id is required")
+    })
+
+    it("should throw error when name is empty", () => {
+        expect(() => {
+            let customer = new Customer("123", "")
+            
+        }).toThrow("Name is required")
+    })
+
+    it("should change name", () => {
+
+        const customer = new Customer("123", "John")
+
+        customer.changeName("Jack")
+        
+        expect(customer.name).toBe("Jack")
+    })
+
+    it("should activate customer", () => {
+
+        const customer = new Customer("1", "Customer 1")
+
+        const address = new Address("Street 1", 123, "06625-458", "São Paulo")
+
+        customer.Address = address
+
+        customer.activate();
+        
+        expect(customer.isActive()).toBe(true);
+    })
+
+    it("should throw error when address is undefined", () => {
+
+        expect(() => {
+            const customer = new Customer("1", "Customer 1")
+
+            customer.activate();
+        }).toThrow("Address is mandatory to active a customer");
+        
+    })
+
+    it("should deactivate customer", () => {
+
+        const customer = new Customer("1", "Customer 1")
+
+        customer.deactivate();
+        
+        expect(customer.isActive()).toBe(false);
+    })
+
+    it("should add reward points", () => {
+
+        const customer = new Customer("1", "Customer 1")
+        expect(customer.rewardPoints).toBe(0);
+
+        customer.addRewardPoints(10);
+        
+        expect(customer.rewardPoints).toBe(10);
+    })
+
+})
